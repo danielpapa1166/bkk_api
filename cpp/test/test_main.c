3fa9880582e90d4b1dc0671302a0ac0b0d77ffab
Dániel Pápa <danielpapa1166@gmail.com>
2026-05-02 11:06:11 +0200

    test runtime measurement

--- cpp/test/test_main.c
+++ cpp/test/test_main.c
@@ -1,11 +1,21 @@
 #include "bkk_uds_client.h"
 #include <stdio.h>
+#include <time.h>
 
 const char * test_stop_id = "F02122";
 
 int main() {
-  bkk_uds_response_t response; 
-  const int res = send_bkk_uds_query(test_stop_id, &response); 
+  bkk_uds_response_t response;
+
+  struct timespec t_start, t_end;
+  clock_gettime(CLOCK_MONOTONIC, &t_start);
+  const int res = send_bkk_uds_query(test_stop_id, &response);
+  clock_gettime(CLOCK_MONOTONIC, &t_end);
+
+  long elapsed_us = (t_end.tv_sec - t_start.tv_sec) * 1000000L
+                  + (t_end.tv_nsec - t_start.tv_nsec) / 1000L;
+  printf("Response time: %ld us\n", elapsed_us);
+
   if(res == 0) {
     printf("Received %d arrivals for stop_id %s\n", response.number_of_arrivals, test_stop_id);
     for(int i = 0; i < response.number_of_arrivals; i++) {
